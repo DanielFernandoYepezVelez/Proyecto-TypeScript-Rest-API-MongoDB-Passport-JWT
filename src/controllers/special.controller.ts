f11391c7931b52c721963a6a1c2ff41578dcf517
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2020-06-14 01:13:24 -0500

    Obteniendo Los Datos Del Payload Del Token Por Medio Del Request Sin Agregar Un Namespace

--- src/controllers/special.controller.ts
+++ src/controllers/special.controller.ts
@@ -3,6 +3,11 @@ import { Request, Response } from "express";
 class SpecialController {
   public async special(req: Request, res: Response): Promise<Response<JSON>> {
     try {
+      /* Aqui Tengo El Valor Del Payload-JWT */
+      const userToken = req.user;
+      console.log(req.user);
+      console.log(userToken);
+
       return res.json({
         ok: true,
         message:
